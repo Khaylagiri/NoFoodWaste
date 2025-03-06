9e6d40f3185516672fafcd6acb2de5363271737c
Budi <64240@kai.id>
2025-03-06 13:38:23 +0700

    upload skeleton

---
+++ resources/views/layouts/admin/partials/sidebar.blade.php
@@ -0,0 +1,21 @@
+<header class="main-nav">
+    <div class="sidebar-user text-center">
+        <img class="img-90 rounded-circle" src="{{asset('assets/images/dashboard/1.png')}}" alt="" />
+        <a href="user-profile"> <h6 class="mt-3 f-14 f-w-600">Emay Walter</h6></a>
+        <p class="mb-0 font-roboto">Human Resources Department</p>
+    </div>
+    <nav>
+        <div class="main-navbar">
+            <div id="mainnav">
+                <ul class="nav-menu custom-scrollbar">
+                    <li class="back-btn">
+                        <div class="mobile-back text-end"><span>Back</span><i class="fa fa-angle-right ps-2" aria-hidden="true"></i></div>
+                    </li>
+                    <li class="dropdown">
+                        <a class="nav-link menu-title" href="{{route('index')}}"><i data-feather="home"></i><span>Home</span></a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</header>
